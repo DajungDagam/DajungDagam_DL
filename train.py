d0b19b6216150d7e4716a64fb08dc1d0d2b73019
Just-love-myself <ehrbs7181@gmail.com>
2024-01-20 18:21:32 +0900

    Mysql DB 연동 성공

--- train.py
+++ train.py
@@ -4,15 +4,45 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import logging
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, Column, Integer, String, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import mysql.connector
+
 
 logging.basicConfig(level=logging.DEBUG)
 
+DATABASE_URL = "mysql+mysqlconnector://root:0000@localhost:3306/dg"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# Post 모델 정의
+class Post(Base):
+    __tablename__ = "post"
+    post_id = Column(Integer, primary_key=True, index=True)
+    post_title = Column(String, index=True)
+
+
+# 데이터베이스 연결 설정
+# Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-# 모델 파일의 경로를 정확하게 지정
-# model_path = 'saved_model/my_model'
-# model = tf.keras.models.load_model(model_path)
 
+# DB 세션 의존성 설정
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# 모델 파일의 경로를 정확하게 지정
 model_path = 'saved_model/my_model'
 model = None
 
@@ -24,10 +54,10 @@ except Exception as e:
     raise HTTPException(status_code=500, detail="Internal Server Error")
 
 # Load the post data
-num_posts = 15
-post_titles = ['전자레인지 판매', '자전거 판매', '책상 판매', '노트북 판매', '의자 판매', '탁자 판매', '램프 판매', '커튼 판매', '침대 판매', '옷장 판매', '신발 판매',
-               '시계 판매', '가방 판매', '키보드 판매', '마우스 판매']
-posts_df = pd.DataFrame({'post_id': range(1, num_posts + 1), 'title': post_titles})
+# num_posts = 15
+# post_titles = ['전자레인지 판매', '자전거 판매', '책상 판매', '노트북 판매', '의자 판매', '탁자 판매', '램프 판매', '커튼 판매', '침대 판매', '옷장 판매', '신발 판매',
+#                '시계 판매', '가방 판매', '키보드 판매', '마우스 판매']
+# posts_df = pd.DataFrame({'post_id': range(1, num_posts + 1), 'title': post_titles})
 
 
 class RecommendationRequest(BaseModel):
@@ -44,23 +74,36 @@ class RecommendationResponse(BaseModel):
 
 
 @app.post("/get_recommendations", response_model=RecommendationResponse)
-def get_recommendations(request: RecommendationRequest):
+def get_recommendations(request: RecommendationRequest, db: Session = Depends(get_db)):
     user_id = request.user_id
     post_id = request.post_id
     interaction = request.interaction
     post_title = request.post_title
     num_recommendations = request.num_recommendations
 
-    # print(user_id, post_id, interaction, post_title, num_recommendations)
     logging.info(
         f"Model input: user_id={user_id}, post_id={post_id}, interaction={interaction}, post_title={post_title}, num_recommendations={num_recommendations}")
+
+    try:
+        # 게시글 개수 가져오기
+        num_posts = db.query(func.count(Post.post_id)).scalar()
+        # 게시글 정보 가져오기
+        posts = db.query(Post.post_id, Post.post_title).all()
+        logging.info(f"num_posts={num_posts}, posts={posts}")
+    except Exception as e:
+        logging.error(f"DB에서 값 가져오기 실패: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+
+    # Pandas DataFrame 생성
+    posts_df = pd.DataFrame(posts, columns=["post_id", "title"])
 
     # Generate recommendations using the loaded model
     post_ids = np.array(list(range(1, num_posts + 1)))
     user_ids = np.array([user_id] * num_posts)
     user_ids = np.expand_dims(user_ids, axis=-1)
     post_ids = np.expand_dims(post_ids, axis=-1)
-    # predictions = model.predict([user_ids, post_ids]).flatten()
     try:
         predictions = model.predict([user_ids, post_ids]).flatten()
     except Exception as e:
